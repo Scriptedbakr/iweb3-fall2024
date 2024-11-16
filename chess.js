d43444b43ed0d5210b7a3ad973ec544d814341de
Scriptedbakr <babatunde.bakr@gmail.com>
2024-11-17 00:53:40 +0100

    added comments to the chess problem set

--- chess.js
+++ chess.js
@@ -1,15 +1,16 @@
-const size = 8;
-let chessBoard = "";
+const size = 8; // size of the grid
+let chessBoard = ""; // the chess board as a string
 
-for (let i = 0; i < size; i++) {
+for (let i = 0; i < size; i++) // iterate over the first loop and the second loop 
+    {
     for (let j = 0; j < size; j++) {
         if ((i + j) % 2 === 0) {
-            chessBoard += " ";
+            chessBoard += " "; // if the sum of the row and column is even, add a space
             } else {
-                chessBoard += "#";
+                chessBoard += "#"; // if the sum of the row and column is odd, add a hash
                 }
         }
-       chessBoard += "\n";
+       chessBoard += "\n"; // add a newline character after each row
 }
 console.log(chessBoard);
 
